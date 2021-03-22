fd38da13ea7767675e4df4b4f88d24fa7c726960
Michael Kieda <mbkieda1959@gmail.com>
2021-03-22 08:38:00 -0400

    Post review changes

--- temperature.sql
+++ temperature.sql
@@ -1,10 +1,12 @@
-SELECT c.year AS year,
+SELECT
+  c.year AS year,
   c.avg_temp AS detroit_avg_temp,
-  AVG(c.avg_temp) OVER (ORDER by c.year  ROWS BETWEEN 10 PRECEDING AND CURRENT ROW) AS detroit_rolling_avg_t,
   g.avg_temp AS global_avg_temp,
-  AVG(g.avg_temp) OVER (ORDER by c.year  ROWS BETWEEN 10 PRECEDING AND CURRENT ROW) AS global_rolling_avg_t
+  AVG(c.avg_temp) OVER (ORDER BY c.year ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) AS detroit_moving_avg_t,
+  AVG(g.avg_temp) OVER (ORDER BY c.year ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) AS global_moving_avg_t
 FROM city_data c
 JOIN global_data g
-ON c.year = g.year
-WHERE c.city = 'Detroit' AND c.country = 'United States'
+  ON c.year = g.year
+WHERE c.city = 'Detroit'
+AND c.country = 'United States'
 ORDER BY 1
